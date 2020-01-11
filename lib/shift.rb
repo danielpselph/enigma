7a22c9f12015f3136833000771725535d5d68cae
Daniel Selph <daniel.p.selph@gmail.com>
2020-01-11 12:55:29 -0700

    Add key_grouping test and method, used stubs to test key values

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,9 +1,20 @@
 class Shift
+  attr_reader :key, :date
 
-  def initialize(key = Array.new(5) {rand(0..9)},
-                date = Time.now.strftime("%d%m%y").to_i)
+  def initialize(key = (Array.new(5) {rand(0..9)}),
+                date = Time.now.strftime("%d%m%y"))
     @key = key
     @date = date
   end
+
+  def key_grouping
+    new_keys = []
+    key.split(//).each_cons(2) do |num|
+      new_keys << num.join
+    end
+    new_keys
+  end
+
+  
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,11 +5,23 @@ require './lib/shift'
 class ShiftTest < Minitest::Test
 
   def setup
-    @shift = Shift.new("02715", "040895")
+    @shift = Shift.new
   end
 
   def test_it_exists
     assert_instance_of Shift, @shift
   end
 
+  def test_it_has_attributes
+    assert_equal Array, @shift.key.class
+    assert_equal 5, @shift.key.length
+    assert_equal String, @shift.date.class
+    assert_equal 6, @shift.date.length
+  end
+
+  def test_keys_can_be_grouped
+    @shift.stubs(:key => "01234")
+    expected = ["01", "12", "23", "34"]
+    assert_equal expected, @shift.key_grouping
+  end
 end
