7beb0f50aa2f19f1f62ea4257f50ace81e61a5ec
Daniel Selph <daniel.p.selph@gmail.com>
2020-01-15 07:38:56 -0700

    Complete encrypt and decrypt runner files

--- encrypt_runner.rb
+++ encrypt_runner.rb
@@ -1,6 +1,5 @@
 require './lib/enigma'
 require './lib/shift'
-# require './test/test_helper'
 
 enigma = Enigma.new
 
@@ -8,10 +7,8 @@ message = File.open(ARGV[0], "r")
 
 incoming_text = message.read
 
+encrypted_message = enigma.encrypt(incoming_text.chomp)
 
-encrypted_message = enigma.encrypt(incoming_text)
-# require "pry"; binding.pry
 file = File.write(ARGV[1], encrypted_message[:encryption])
 
-# require 'pry'; binding.pry
 puts "Created 'encrypted.txt' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
